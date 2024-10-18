96a9c17c085112c3c23c215968e89430d17180a9
unknown <wk28835@gmail.com>
2024-10-19 00:59:50 +0500

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Link from "next/link";
-import Resume from "./components/resume";
+
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -27,13 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-      <>
-            {/* Navigation Bar */}
-            
-
-            {/* Resume Component */}
-            
-        </>
+      
 
       
         {children}
